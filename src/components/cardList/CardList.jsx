2701deb55b9f7140994e0e173001b791846690a0
Simon Yoo <yoosghn0@gmail.com>
2023-10-30 17:23:55 +1300

    nvm

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './cardList.module.css';
 import Pagination from '../pagination/Pagination';
+import Image from 'next/image';
 const CardList = () => {
   return (
     <div className={styles.container}>
-      CardList
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt='' fill />
+          </div>
+          <div className={styles.textContainer}></div>
+        </div>
+      </div>
       <Pagination />
     </div>
   );
