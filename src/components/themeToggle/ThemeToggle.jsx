c1d66139f2ca55788c0cfa178c4c5b13dee0bc90
Simon Yoo <yoosghn0@gmail.com>
2023-10-30 15:54:05 +1300

    added darkmode

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -10,14 +10,22 @@ const ThemeToggle = () => {
   console.log(theme);
 
   return (
-    <div className={styles.container} onClick={toggle}>
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === 'dark'
+          ? { backgroundColor: 'white' }
+          : { backgroundColor: '#0f172a' }
+      }
+    >
       <Image src='/moon.png' alt='' width={14} height={14} />
       <div
         className={styles.ball}
         style={
           theme === 'dark'
-            ? { left: 1, background: '#0f172a' }
-            : { right: 1, background: 'white' }
+            ? { left: 3, background: '#0f172a' }
+            : { right: 3, background: 'white' }
         }
       ></div>
       <Image src='/sun.png' alt='' width={14} height={14} />
